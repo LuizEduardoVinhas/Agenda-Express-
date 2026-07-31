34aa44ab159fd45ab285b58924cd16d02d4051ac
Luiz Eduardo Lima Coimbra Vinhas <152452740+Eduarod27@users.noreply.github.com>
2026-07-31 19:25:10 -0300

    fix: corrige autenticação (CSRF, hash de senha, login/logout) e mensagens de validação

--- routes.js
+++ routes.js
@@ -3,11 +3,13 @@ const route = express.Router();
 const homeController = require('./src/controllers/homeController');
 const loginController = require('./src/controllers/loginController');
 
-
 // rotas home
-route.get('/index', homeController.index);
+route.get('/', homeController.index);
 
 // rotas login
-route.get('/index/login', loginController.index)
+route.get('/login/index', loginController.index)
+route.post('/login/register', loginController.register)
+route.post('/login/login', loginController.login)
+route.get('/login/logout', loginController.logout)
 
 module.exports = route;
